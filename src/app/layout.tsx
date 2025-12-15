59a0288a7b7bb76b9ea8e6c8c4c57f6d2131e106
InvarGervi123 <InvarGervi1234@gmail.com>
2025-12-15 19:52:38 +0200

    joke

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 
 export const metadata = {
-  title: 'MiniTube',
+  title: 'YamShamenTube',
   description: 'A small YouTube-style clone (Next.js + Supabase)'
 }
 
